3a92b42eb1fb5e413c2377197c8b3bce90bbc39b
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-30 18:58:42 +0530

    feat; Create controller method to register new user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './models/dto/create-user.dto';
+import { User } from './models/entities/user.entity';
 
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+
+  constructor(private readonly usersService: UsersService) { }
+
+  @Post()
+  createUser(@Body() createUserDto:CreateUserDto): Promise<User> {
+    return this.usersService.createUser(createUserDto);
+  }
 }

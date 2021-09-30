39ec560faaa26f35cd037f4d87e887c6fd0a24e2
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-30 18:58:03 +0530

    feat; Create function to register new user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './models/entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './models/dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    constructor(@InjectRepository(User) private usersRepositiry: Repository<User>) { }
+
+    async createUser(createUserDto:CreateUserDto): Promise<User> {
+        const user = await this.usersRepositiry.create(createUserDto);
+        await user.save();
+
+        delete user.password;
+        return user;
+    }
+}

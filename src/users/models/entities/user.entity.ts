402fc10440381f1d62ff610d4a455b2bed3105ec
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-30 18:23:34 +0530

    feat: Create create-ser-dto.ts

---
+++ src/users/models/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity, Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeInsert } from "typeorm";
+import * as bcrypt from 'bcryptjs';
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique:true })
+    email: string
+
+    @Column()
+    password: string;
+
+    @Column()
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @Column()
+    @UpdateDateColumn()
+    updatedAt: Date;
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+
+    async validatePassword(password: string): Promise<boolean> {
+        return bcrypt.compare(password, this.password);
+    }
+}

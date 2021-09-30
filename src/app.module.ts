1077a73642c41df58d60955175608a807ad0e345
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-30 16:39:52 +0530

    feat: Create Users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       database: process.env.DB_NAME,
       entities: ['dist/src/**/*.entity.js'],
       synchronize: true,
-    })
+    }),
+    UsersModule
   ],
   controllers: [],
   providers: [],
